c4e63471a956ee20a47d8a3fdfbb5998045736c2
Sergey Bulavskiy <sergey.bulavskiy@gmail.com>
2021-02-19 00:35:41 +0700

    Tests for args helper added, implementation adjusted.

--- spotify_unsub_fake_artists/args_helpers.py
+++ spotify_unsub_fake_artists/args_helpers.py
@@ -15,27 +15,36 @@ def parse_lastfm_credentials_from_args(argv):
     lastfm_username = ''
     lastfm_password = ''
     help_string = (
-        f'usage: main.py  [-{CONST_USERNAME_SHORT} | -{CONST_USERNAME_LONG}] <username>'
-        f' [-{CONST_PASSWORD_SHORT} | -{CONST_PASSWORD_LONG}] <password>'
-        '\nScript requires lastfm credentials, '
+        f'usage: main.py  [-{CONST_USERNAME_SHORT} | --{CONST_USERNAME_LONG}] <username>'
+        f' [-{CONST_PASSWORD_SHORT} | --{CONST_PASSWORD_LONG}] <password>'
+        '\nScript requires lastfm credentials, both username and password is obligatory. '
         'please specify them like shown in usage above.')
 
-    try:
-        opts, _ = getopt.getopt(
-            argv,
-            f"h{CONST_USERNAME_SHORT}:{CONST_PASSWORD_SHORT}:",
-            [f"{CONST_USERNAME_LONG}=", f"{CONST_PASSWORD_LONG}="])
-    except getopt.GetoptError:
+    def exit_with_help_message():
         print(help_string)
         sys.exit(2)
+
+    try:
+        opts, _ = getopt.getopt(argv, f'h{CONST_USERNAME_SHORT}:{CONST_PASSWORD_SHORT}:',
+                                [f"{CONST_USERNAME_LONG}=", f"{CONST_PASSWORD_LONG}="])
+    except getopt.GetoptError:
+        exit_with_help_message()
+
+    if len(opts) == 0:
+        exit_with_help_message()
+
     for opt, arg in opts:
         if opt == '-h':
             print(help_string)
             sys.exit()
-        elif opt in (f"-{CONST_USERNAME_SHORT}", "-{CONST_USERNAME_LONG}"):
+        elif opt in (f"-{CONST_USERNAME_SHORT}", f"-{CONST_USERNAME_LONG}"):
             lastfm_username = arg
-        elif opt in (f"-{CONST_PASSWORD_SHORT}", "-{CONST_PASSWORD_LONG}"):
+        elif opt in (f"-{CONST_PASSWORD_SHORT}", f"-{CONST_PASSWORD_LONG}"):
             lastfm_password = arg
+
+    if lastfm_password == '' or lastfm_password == '':
+        exit_with_help_message()
+
     return lastfm_username, lastfm_password
 
 
--- tests/test_terminal_args_parser.py
+++ tests/test_terminal_args_parser.py
@@ -1,23 +1,62 @@
 import unittest
 from unittest import TestCase
+from unittest.mock import patch
 from spotify_unsub_fake_artists import args_helpers
+
+expected_help_message = (
+    f'usage: main.py  [-u | --username] <username>'
+    f' [-p | --password] <password>'
+    '\nScript requires lastfm credentials, both username and password is obligatory. '
+    'please specify them like shown in usage above.')
 
 
 class ArgsHelpersTests(TestCase):
-    def test_empty_args_raises_error(self):
-        self.fail()
+    def test_empty_args_system_exits_with_code_2(self):
+        with self.assertRaises(SystemExit) as err:
+            args_helpers.parse_lastfm_credentials_from_args([])
+        self.assertEqual(err.exception.code, 2)
 
-    def test_help_arg_prints_out_help_message(self):
-        self.fail()
+    @patch('builtins.print')
+    def test_empty_args_prints_help_message(self, mock_print):
+        with self.assertRaises(SystemExit):
+            args_helpers.parse_lastfm_credentials_from_args([])
+        mock_print.assert_called_with(expected_help_message)
 
-    def test_username_arg_missing_raises(self):
-        self.fail()
+    def test_help_arg_system_exits_without_error_code(self):
+        with self.assertRaises(SystemExit) as err:
+            args_helpers.parse_lastfm_credentials_from_args(['-h'])
+        self.assertEqual(None, err.exception.code)
 
-    def test_password_arg_missing_raises(self):
-        self.fail()
+    @patch('builtins.print')
+    def test_help_arg_prints_out_help_message(self, mock_print):
+        with self.assertRaises(SystemExit):
+            args_helpers.parse_lastfm_credentials_from_args(['-h'])
+        mock_print.assert_called_with(expected_help_message)
+
+    @patch('builtins.print')
+    def test_username_missing_raises(self, mock_print):
+        with self.assertRaises(SystemExit):
+            args_helpers.parse_lastfm_credentials_from_args(['-u', 'usrname'])
+        mock_print.assert_called_with(expected_help_message)
+
+    @patch('builtins.print')
+    def test_password_missing_raises(self, mock_print):
+        with self.assertRaises(SystemExit):
+            args_helpers.parse_lastfm_credentials_from_args(['-p', 'pssw'])
+        mock_print.assert_called_with(expected_help_message)
 
     def test_credentials_provided_correctly_returns(self):
-        self.fail()
+        expected_username = 'some_username'
+        expected_password = 'some_password'
+
+        actual_username, actual_password = args_helpers. \
+            parse_lastfm_credentials_from_args(["-u", f"{expected_username}",
+                                                "-p", f"{expected_password}"])
+
+        self.assertEqual(expected_username, actual_username)
+        self.assertEqual(expected_password, actual_password)
+
+    #TODO: Add tests for long parameter names.
 
 
 if __name__ == '__main__':
